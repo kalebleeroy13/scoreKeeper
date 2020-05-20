ccdc389a377fb09803744b7b39b0782f961644e9
khuunter@yahoo.com <khuunter@yahoo.com>
2020-05-20 10:09:05 -0400

    button, display and score variables added

--- scoreKeeper.js
+++ scoreKeeper.js
@@ -1,1 +1,18 @@
-alert("howdy dowdy")
+let p1Button = document.querySelector("#p1");
+let p2Button = document.getElementById("p2");
+let p1Display = document.querySelector("#p1Display")
+let p2Display = document.querySelector("#p2Display")
+let p1Score = 0;
+let p2Score = 0;
+
+
+
+p1Button.addEventListener("click", function() {
+  p1Score++;
+  p1Display.textContent = p1Score
+});
+
+p2Button.addEventListener("click", function() {
+  p2Score++;
+  p2Display.textContent = p2Score
+});
